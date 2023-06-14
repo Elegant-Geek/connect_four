2e9d219d0998869a3f3f59aa9b76484658834f4d
Jamie Clark <clarkcjamie@gmail.com>
2023-06-14 04:07:30 -0400

    add comment

--- connect_four.rb
+++ connect_four.rb
@@ -1,4 +1,5 @@
 # 8:48pm on 6.13.23 is start of this project connect four
+# ended main code at 4:04am, so now all I gotta do is write the tests for it! Yay!
 require_relative './test_big_arrays.rb'
 class Game
   attr_reader :board, :players
